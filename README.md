3325398f7289d76a639bc1867d8f040a70409a57
steven_c <steven.c.projects@gmail.com>
2015-09-01 21:23:01 -0500

    Changed to AGPL. Added opt depd to setup. Centralized headers.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+gentrify  Copyright (C) 2015  Steven Cutting - License: gentrify/LICENSE
+
 Makes misc files usable for nlp.
--- setup.py
+++ setup.py
@@ -1,40 +1,56 @@
 # -*- coding: utf-8 -*-
 """
-gentrify  Copyright (C) 2015  Steven Cutting - License: gentrify/LICENSE
+gentrify  Copyright (C) 2015  Steven Cutting - License AGPL: gentrify/LICENSE
 """
+
+from gentrify import(__title__, __version__, __status__)
+
+
 from setuptools import setup, find_packages
 
 with open("README.md") as fp:
     THE_LONG_DESCRIPTION = fp.read()
 
+TESTDEPEND = ['pytest']
+# DEVDEPEND = []
+# Work on making python-magic optional.
+FULLDEPEND = ['click', 'cchardet', 'python-magic', 'python-docx', 'xlrd']
 
-setup(
-    name='gentrify',
-    version='0.1.0',
-    license='GNU GPL v3+',
-    description="Makes misc files usable for nlp.",
-    long_description=THE_LONG_DESCRIPTION,
-    classifiers=['Topic :: NLP',
-                 'Topic :: Text cleaning',
-                 'Intended Audience :: Developers',
-                 'Intended Audience :: Data Scientists',
-                 'Development Status :: 3 - Alpha',
-                 'Programming Language :: Python :: 2.7',
-                 'License :: GNU GPL v3+',
-                 ],
-    keywords='nlp encoding text plaintext preprocessing',
-    author='Steven Cutting',
-    author_email='steven.e.cutting@linux.com',
-    packages=find_packages(exclude=('scripts', 'tests')),
-    # zip_safe=False,
-    install_requires=['chardet',
-                      'ftfy>=4,<5',
-                      'arrow',
-                      'smart_open',  # TODO (steven_c) phase out smart_open
-                      'beautifulsoup4',
-                      'pathlib',
-                      'docx',
-                      'pdfminer',
-                      'lxml',
-                      ],
-    )
+setup(name=__title__,
+      version=__version__,
+      license='GNU AGPL',
+      description="Makes misc files usable for nlp.",
+      long_description=THE_LONG_DESCRIPTION,
+      classifiers=['Topic :: NLP :: Text cleaning',
+                   'Topic :: NLP :: Initial processing',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Data Scientists',
+                   'Development Status ::' + __status__,
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
+                   'License :: GNU AGPL',
+                   'Copyright :: gentrify  Copyright (C) 2015  Steven Cutting',
+                   'Status ::' + __status__,
+                   ],
+      keywords='nlp encoding text plaintext preprocessing',
+      author='Steven Cutting',
+      author_email='steven.e.cutting@linux.com',
+      packages=find_packages(exclude=('scripts', 'tests')),
+      # zip_safe=False,
+      install_requires=['chardet',
+                        'ftfy>=4,<5',
+                        'arrow',
+                        'smart_open',  # TODO (steven_c) phase out smart_open
+                        'beautifulsoup4',
+                        'pathlib',
+                        'docx',
+                        'pdfminer',
+                        'lxml',
+                        'unidecode',
+                        ],
+      extras_require={
+          # 'dev': DEVDEPEND,
+          'full': FULLDEPEND,
+          'test': TESTDEPEND,
+      }
+      )
